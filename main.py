815f4c56a742df44820ff03318d401955d9ae0ac
nycfinest <lsrpmecca1@gmail.com>
2021-03-09 20:19:17 -0500

    Update

---
+++ main.py
@@ -0,0 +1,161 @@
+# store = input("Where do you shop for clothes: ")
+# print("I shop at", store)
+
+# theres diff datatypes too and if u wanna comment out a line, just select it and hit control / this wont be considered code 
+
+# this selects the first element 0 is considered first
+# food = ['apple', 'banana', 'dingdong']
+# print(food[2])
+
+# dictionary is another data type
+
+# car = {
+#     "name": "ford",
+#     "brand": "explorer",
+#     "speed": 200,
+#     "year": 2018
+#     }
+
+# # .get is a method similar to food[0]
+
+# print(car.get("brand"))
+
+# freeruns = {
+#     "name": "Freeruns",
+#     "brand": "Nike",
+#     "year": 2021
+# }
+
+# jordans = {
+#     "name": "Air",
+#     "brand": "Jordan",
+#     "year": 2017
+# }
+
+# sketchers = {
+#     "name": "High",
+#     "brand": "Tops",
+#     "year": 1992
+# }
+ 
+# print(shoe.get("name"))
+
+# print(shoe.get("name"), "are made by", shoe.get("brand"), "and it was released in", shoe.get("year"))
+
+# print(freeruns.get("brand"))
+
+# shoes = [{"name": "Freeruns", "brand": "Nike", "year": 2021}, {"name": "Air","brand": "Jordan","year": 2017}, {"name": "High","brand": "Tops","year": 1992} ]
+
+# print(shoes[1].get("brand"))
+
+
+
+# Flow control
+
+# x = 2
+# y = 4
+
+# if(x > y):
+#     print(x, "is greater than", y)
+    
+# elif(x == y):
+#     print(x, "is equivalent to", y)
+# else:
+#     print(x, "is less than", y)
+    
+    
+# freeruns = {
+#     "name": "Freeruns",
+#     "brand": "Nike",
+#     "year": 2020,
+#     "brand": True
+# }
+
+
+# if(freeruns.get("year") > 2020):
+#     print(freeruns.get("name"), "is a new shoe" )
+    
+    
+# elif(freeruns.get("year") == 2020):
+#     print(freeruns.get("name"), "is a used shoe")
+
+        
+# else:
+#     print(freeruns.get("name"), "is an old shoe")
+
+    
+# #
+# if(freeruns.get("brand") == True):
+#         print("Also, it is brand!")
+# else:
+#     print("It is not a brand..")
+    
+    
+    
+    
+# store = input("Where do you shop for clothes: ")
+# print("I shop at", store)
+
+
+
+
+
+# #dont touch this
+# owner = "akash"
+
+        
+# user_input = input("What is your name: ")
+
+# user_input = user_input.lower()
+
+# if(user_input == owner):
+#     print("Welcome", owner)
+# else:
+#     print("imposter")
+
+
+# print(user_input)
+
+
+
+# fname = "John"
+# lname = "Cena"
+# fullname = fname + "_" + lname
+
+# print(fullname)
+
+
+
+# user_input = input("What is your name: ")
+
+# first_letter = user_input[0].upper()
+
+# everything_else = user_input[1:].lower()
+
+# correct_name = first_letter + everything_else
+
+# print(correct_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ stock.py
@@ -0,0 +1,70 @@
+user_input = input("Hello, what is your name? ")
+
+first_letter = user_input[0].upper()
+
+everything_else = user_input[1:].lower()
+
+correct_name = first_letter + everything_else
+
+print("Hello,", correct_name) 
+
+# name part finished
+
+brand_question = input("What is your favorite brand? ")
+
+
+stock = {
+    "name": "Freeruns",
+    "brand": "Nike",
+    "year": 2019,
+    "brand": True,
+}    
+
+if(stock.get("brand") == True):
+    print(stock.get("name"), "is currently in stock!")
+else:
+    print("Sorry, piss off!")
+
+
+
+
+
+
+# if(freeruns.get("year") > 2020):
+#     print(freeruns.get("name"), "is a new shoe" )
+    
+    
+# elif(freeruns.get("year") == 2020):
+#     print(freeruns.get("name"), "is a used shoe")
+
+        
+# else:
+#     print(freeruns.get("name"), "is an old shoe")
+# #
+# if(freeruns.get("brand") == True):
+#         print("Also, it is brand!")
+# else:
+#     print("It is not a brand..")
+
+
+
+# Nike = {
+#     "name": "Freeruns",
+#     "year": 2020,
+#     "brand": True
+# }
+# Jordan = {
+#     "name": "Air",
+#     "year": 2018,
+#     "brand": True
+# }
+# Sketchers = {
+#     "name": "High-Top",
+#     "year": 2021,
+#     "brand": True
+# }
+# Vans = {
+#     "name": "Oldschool",
+#     "year": 2016,
+#     "brand": False
+# }
